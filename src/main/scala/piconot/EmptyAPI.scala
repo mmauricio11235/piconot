348f90262ba8bca417baf16d5e5bc04609832569
Ben Wiedermann <benw@cs.hmc.edu>
2014-09-20 01:46:33 -0700

    Fixing bug?

--- src/main/scala/piconot/EmptyAPI.scala
+++ src/main/scala/piconot/EmptyAPI.scala
@@ -17,44 +17,49 @@ object PiconotSyntax {
         val maze = Maze(io.Source.fromFile(mazeFilename).getLines().toList)
         
         val rules = List( Rule(State("0"), 
-                               Surroundings(Anything, Anything, Open, Anything), 
-                               West,
-                               State("0")),
-                               
-                          Rule(State("0"), 
-                               Surroundings(Open, Anything, Blocked, Anything), 
-                               North,
-                               State("1")), 
-                               
-                          Rule(State("1"), 
-                               Surroundings(Open, Anything, Anything, Anything), 
-                               North,
-                               State("1")),
-                        
-                          Rule(State("1"), 
-                               Surroundings(Blocked, Anything, Anything, Open), 
-                               South,
-                               State("2")),
-                        
-                          Rule(State("2"), 
-                               Surroundings(Anything, Anything, Anything, Open), 
-                               South,
-                               State("2")),
-                        
-                          Rule(State("2"), 
-                               Surroundings(Anything, Open, Anything, Blocked), 
-                               East,
-                               State("3")),
-                               
-                          Rule(State("3"), 
-                               Surroundings(Open, Anything, Anything, Anything), 
-                               North,
-                               State("3")),
-                        
-                          Rule(State("3"), 
-                               Surroundings(Blocked, Open, Anything, Anything), 
-                               East,
-                               State("2"))            
+               Surroundings(Anything, Anything, Open, Anything), 
+               West,
+               State("0")),
+               
+          Rule(State("0"), 
+               Surroundings(Open, Anything, Blocked, Anything), 
+               North,
+               State("1")), 
+               
+          Rule(State("0"), 
+               Surroundings(Blocked, Open, Blocked, Anything), 
+               South,
+               State("2")), 
+               
+          Rule(State("1"), 
+               Surroundings(Open, Anything, Anything, Anything), 
+               North,
+               State("1")),
+        
+          Rule(State("1"), 
+               Surroundings(Blocked, Anything, Anything, Open), 
+               South,
+               State("2")),
+        
+          Rule(State("2"), 
+               Surroundings(Anything, Anything, Anything, Open), 
+               South,
+               State("2")),
+        
+          Rule(State("2"), 
+               Surroundings(Anything, Open, Anything, Blocked), 
+               East,
+               State("3")),
+               
+          Rule(State("3"), 
+               Surroundings(Open, Anything, Anything, Anything), 
+               North,
+               State("3")),
+        
+          Rule(State("3"), 
+               Surroundings(Blocked, Open, Anything, Anything), 
+               East,
+               State("2"))            
         )
        
         new Picobot(maze, rules).run()
