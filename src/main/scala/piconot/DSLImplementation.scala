81ca1302ba8604d4738745777a26412bee07ad00
Ellen Hui <ellenfkh@gmail.com>
2014-09-27 16:06:15 -0700

    clean up

--- src/main/scala/piconot/DSLImplementation.scala
+++ src/main/scala/piconot/DSLImplementation.scala
@@ -24,20 +24,20 @@ object DSLImplementation {
   var globalRules:List[Rule] = List()
 
   /**
-   *  Global number will keep track of the number of states there are
-   */
-	var numberOfStates = 0
+    *  Global number will keep track of the number of states there are
+    */
+  var numberOfStates = 0
 
-	var stringToStateMap: Map[String, MyState] = Map[String, MyState]()
+  var stringToStateMap: Map[String, MyState] = Map[String, MyState]()
 
   /**
-   * Need a way to know how many states there are.
-   */
+    * Need a way to know how many states there are.
+    */
 
   class MyState(stateName: String) {
 
-	  // We map the given name to the state itself
-	stringToStateMap += stateName -> this
+    // We map the given name to the state itself
+    stringToStateMap += stateName -> this
 
     // Assigning a number to the string name
     // NEED TO DO CHECK IF IT ALREADY EXISTS FIRST
@@ -47,13 +47,13 @@ object DSLImplementation {
 
 
     /**
-     * Need to go from dummy state (strings) to PicoBot objects
-     * 
-     * Need to create dummy state that hasn't been made yet. 
-     */
+      * Need to go from dummy state (strings) to PicoBot objects
+      * 
+      * Need to create dummy state that hasn't been made yet. 
+      */
     def createRule(surroundings: Surroundings,
-                   direction: MoveDirection,
-                   newState: State) {
+      direction: MoveDirection,
+      newState: State) {
 
 
       var testState = State(this.stateNumber.toString())
@@ -64,92 +64,66 @@ object DSLImplementation {
 
   // Creates a new state with
   // Needs to check if state already exists. Don't add if it already does
-  def inState(newState: =>  String)(rules: => Unit){
-    println(newState)
-    //if (stringToStateMap.contains(newState)){
-    //	val stateCreate = stringToStateMap(newState)
-    //}
-    //else{
-    //Only do this step if doesnt exist in map
-      val stateCreate = new MyState(newState)
-    //}
-	  stateCreate.createRule(surr,direc,nextS)
+  def inState(newState: =>  String)(rules: => Unit) {
+    val stateCreate = new MyState(newState)
+    stateCreate.createRule(surr,direc,nextS)
   }
 
-    var surr: Surroundings = Surroundings(Anything, Anything, Anything, Anything)
-    var direc: MoveDirection = North
-    var nextS: State = State("0")
+  var surr: Surroundings = Surroundings(Anything, Anything, Anything, Anything)
+  var direc: MoveDirection = North
+  var nextS: State = State("0")
 
+  def surroundedBy(surroundings: String)(x: => Unit) = {
+    println("GOT TO surroundedBy")
 
-    def surroundedBy(surroundings: String)(x: => Unit) = {
-      println("GOT TO surroundedBy")
-
-      def charToSurr(x:Char):RelativeDescription = {
-        x match {
-          case close:Char if "NEWS".contains(close) => Blocked
-          case 'x' => Open
-          case '*' => Anything
-        }
+    def charToSurr(x:Char):RelativeDescription = {
+      x match {
+        case close:Char if "NEWS".contains(close) => Blocked
+        case 'x' => Open
+        case '*' => Anything
       }
-
+    }
 
     val tmp = surroundings.toList.map(y => charToSurr(y))
     val semanticSurroundings = Surroundings(tmp(0), tmp(1), tmp(2), tmp(3))
 
-
     surr = semanticSurroundings
-
     x
-	}
+  }
 
 
-    def thenMove(direction: String, newState: String) = {
-      def strToMoveDir(x:String):MoveDirection = {
-        x match {
-          case "N" => North
-          case "E" => East
-          case "W" => West
-          case "S" => South
-          case "X" => StayHere
-        }
+  def thenMove(direction: String, newState: String) = {
+    def strToMoveDir(x:String):MoveDirection = {
+      x match {
+        case "N" => North
+        case "E" => East
+        case "W" => West
+        case "S" => South
+        case "X" => StayHere
       }
+    }
 
-      //var nextState:MyState;
+    //var nextState:MyState;
 
-      // make new MyState if i haven't already (dummy) otherwise just get from
-      // map
-      if(stringToStateMap.contains(newState)) {
-        val nextState = stringToStateMap(newState)
-        val semanticNewState = State(nextState.stateNumber.toString())
-        val moveDir = strToMoveDir(direction)
-        direc = moveDir
-        nextS = semanticNewState
-      } else {
-        val nextState = new MyState(newState);
-        val semanticNewState = State(nextState.stateNumber.toString())
-        val moveDir = strToMoveDir(direction)
-        direc = moveDir
-        nextS = semanticNewState
-
-      }
-
-      // Parse directions/states
-      // val semanticNewState = State(nextState.stateNumber.toString())
-      // val moveDir = strToMoveDir(direction)
-
-      // return (moveDir, semanticNewState)
-
-      /*
-      "NExx" -> Surroundings(Blocked, Blocked, Open, Open)
-      "NEWS" -> Blocked 
-      "x" -> Open
-      "*" -> Anything
-      */
+    // make new MyState if i haven't already (dummy) otherwise just get from
+    // map
+    if(stringToStateMap.contains(newState)) {
+      val nextState = stringToStateMap(newState)
+      val semanticNewState = State(nextState.stateNumber.toString())
+      val moveDir = strToMoveDir(direction)
+      direc = moveDir
+      nextS = semanticNewState
+    } else {
+      val nextState = new MyState(newState);
+      val semanticNewState = State(nextState.stateNumber.toString())
+      val moveDir = strToMoveDir(direction)
+      direc = moveDir
+      nextS = semanticNewState
 
     }
+  }
   def getRules() = {
     println(globalRules(0))
     globalRules
   }
-
 }
